91da378f142d45dcf8106b24f2a0af752761cd74
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-21 08:32:08 +0400

    implement Inatra

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,25 @@
 module Inatra
-  class << self
-    def routes; end
+  @@handlers = {}
 
-    def call(env); end
+  class << self
+    def routes(&block)
+      instance_eval(&block)
+    end
+
+    def call(env)
+      handle_request(env['REQUEST_METHOD'], env['PATH_INFO'])
+    end
+
+    private
+
+    def get(path, &block)
+      @@handlers['get'] ||= {}
+      @@handlers['get'][path] = -> { block.call }
+    end
+
+    def handle_request(method, path)
+      handler = @@handlers.dig(method.downcase, path)
+      handler.call if handler
+    end
   end
 end
--- test/exercise/rackup/test.rb
+++ test/exercise/rackup/test.rb
@@ -6,12 +6,10 @@ require_relative './my_app'
 
 class RackTest < Test::Unit::TestCase
   def test_it_says_hello_world
-    omit do
-      browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
-      browser.get '/hello'
-      assert browser.last_response.ok?
-      assert_equal 'Hello World', browser.last_response.body
-    end
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.get '/hello'
+    assert browser.last_response.ok?
+    assert_equal 'Hello World', browser.last_response.body
   end
 
   def test_it_pongs
